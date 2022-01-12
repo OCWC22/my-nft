62fad20b9a464bea2fe519cb4283e5b1b57defbb
William Chen <57119977+OCWC22@users.noreply.github.com>
2022-01-11 22:35:23 -0800

    connect contract made in part 1

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -3,3 +3,8 @@ const API_URL = process.env.API_URL
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3")
 const web3 = createAlchemyWeb3(API_URL)
 const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json")
+
+
+const contractAddress = "0xEA73Ea9be491368f5B185525Bc0EEf9a873De636"
+
+const nftContract = new web3.eth.Contract(contract.abi, contractAddress)
