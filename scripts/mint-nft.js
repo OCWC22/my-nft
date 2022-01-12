0972da346e8239d59047789c9beb822e97a47946
William Chen <57119977+OCWC22@users.noreply.github.com>
2022-01-11 22:40:41 -0800

    updated mintNFT

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -1,10 +1,24 @@
-require("dotenv").config()
-const API_URL = process.env.API_URL
-const { createAlchemyWeb3 } = require("@alch/alchemy-web3")
-const web3 = createAlchemyWeb3(API_URL)
-const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json")
+require("dotenv").config();
+const API_URL = process.env.API_URL;
+const PUBLIC_KEY = process.env.PUBLIC_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(API_URL);
 
-const contractAddress = "0xEA73Ea9be491368f5B185525Bc0EEf9a873De636"
-
+const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json");
+const contractAddress = "0xEA73Ea9be491368f5B185525Bc0EEf9a873De636";
 const nftContract = new web3.eth.Contract(contract.abi, contractAddress)
+
+async function mintNFT(tokenURI) {
+    const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, 'latest'); //get latest nonce
+
+  //the transaction
+    const tx = {
+      'from': PUBLIC_KEY,
+      'to': contractAddress,
+      'nonce': nonce,
+      'gas': 500000,
+      'data': nftContract.methods.mintNFT(PUBLIC_KEY, tokenURI).encodeABI()
+    };
+  }
